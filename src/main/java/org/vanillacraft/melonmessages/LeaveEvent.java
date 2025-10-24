21299a9f0b9dd2a449e5599c07a464707fdf2e58
Dylan Rogers <opendylan@proton.me>
2025-10-24 19:01:13 +0100

    remove redundant import statement

--- src/main/java/org/vanillacraft/melonmessages/LeaveEvent.java
+++ src/main/java/org/vanillacraft/melonmessages/LeaveEvent.java
@@ -5,7 +5,6 @@ import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class LeaveEvent implements Listener {
